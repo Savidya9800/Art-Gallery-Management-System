ee9e332e9132093ba9994cc47152fcf2de79ed40
Savidya Jayalath <savidyajayalath@gmail.com>
2024-08-28 12:03:38 +0530

    Data Base name change

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -12,7 +12,7 @@ app.use("/artWorks", router );
 
 //DB Connection
 //DB pw-: ohYTKpIAkkGLhNTd
-mongoose.connect("mongodb+srv://admin:ohYTKpIAkkGLhNTd@cluster0.omv4o.mongodb.net/Artwork_DB")
+mongoose.connect("mongodb+srv://admin:ohYTKpIAkkGLhNTd@cluster0.omv4o.mongodb.net/ArtGallery_DB")
 .then(() => console.log("Connected to MongoDB"))
 .then(() => {
     app.listen(5000);
