59025d460584d4f0ec77eefdf2ac5a41acce3acf
Mayomibhagya <mayomibhagya.mb@gmail.com>
2024-09-30 21:24:26 +0530

    Revert "gg4"
    This reverts commit b5f309005fad7da91d95d86103eea0d4bb94082b.

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const routerinv = require("./Routes/inventoryRouter"); //Inventory Manager
+<<<<<<< HEAD
 const path = require("path");
 const multer = require("multer");
 
-//Artwork-manager
-const router = require("./Routes/artWorkRoutes"); 
+const router = require("./Routes/artWorkRoutes");
+const routerinv = require("./Routes/inventoryRouter");
+
+=======
+const routerinv = require("./Routes/inventoryRouter"); //Inventory Manager
+const router = require("./Routes/artWorkRoutes"); //Artwork-manager
 const inquiryrouter = require("./Routes/inquiryRoutes"); //Inquiry-manager
+>>>>>>> c8bd906e6040495364200c0c9beee050ec5e549a
 
 const app = express();
 const cors = require("cors");
@@ -14,9 +19,14 @@ const cors = require("cors");
 //Middleware
 app.use(express.json());
 app.use(cors());
+<<<<<<< HEAD
+app.use("/artWorks", router);
+app.use("/inventory", routerinv); //Mayomi
+=======
 
 //Inventory Manager
 app.use("/inventory", routerinv);//Mayomi
+>>>>>>> c8bd906e6040495364200c0c9beee050ec5e549a
 
 //Artwork-manager
 app.use("/artWorks", router);
